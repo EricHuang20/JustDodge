22ab4974d5a4656eef8b8dcfea399f6f576ab978
EricHuang20 <sphericyt@gmail.com>
2020-05-20 19:21:51 -0400

    Added Smiting

--- Game.py
+++ Game.py
@@ -11,7 +11,8 @@ baronImage = pygame.transform.scale(baronImage, (200, 300))
 
 font = pygame.font.Font('Font.ttf', 44)
 health = 10000
-
+smiteDmg = 700
+smiteable = True
 x = 400
 y = 400
 xVel = 0
@@ -29,12 +30,16 @@ def convert(variable, leftMin, leftMax, rightMin, rightMax):
     return rightMin + (valueConverted * rightSpan)
 
 def healthbar(curHealth):
-
     damageScale = convert(curHealth, 1, 10000, 0, 248)
     pygame.draw.rect(screen, (255,255,255), (500,200,250,50), 2)
     healthText = font.render(str(curHealth), True, (255, 0, 0))
     pygame.draw.rect(screen, (255, 0, 0), (502, 202, damageScale, 48), 0)
     screen.blit(healthText, (575, 150))
+    screen.blit(baronImage, (530, 300))
+
+def removeBaron():
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 800), 0)
+
 
 def move(xDist, yDist, spd):
 
@@ -85,42 +90,55 @@ def move(xDist, yDist, spd):
                     return xVel, yVel
 
 
-
-
-
 active = True
 
 
 timeElapsed = 0
 clock = pygame.time.Clock()
 while active:
+    mX, mY = pygame.mouse.get_pos()
     screen.fill((0,0,0))
     dt = clock.tick()
+    healthbar(health)
 
     timeElapsed += dt
+    if health > 0:
+        if timeElapsed > 300:
+            damageValue = random.randint(100, 400)
+            health -= damageValue
+            timeElapsed = 0
 
-    if timeElapsed > 300:
-        damageValue = random.randint(100, 600)
-        health -= damageValue
-        timeElapsed = 0
+    elif health <= 0:
+        smiteable = False
+        removeBaron()
 
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             active = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 3:
-                mX, mY = pygame.mouse.get_pos()
+
                 xDist = mX - x
                 yDist = mY - y
                 xVel, yVel = move(xDist, yDist, speed)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                if smiteable:
+                    if health < smiteDmg:
+                        health -= smiteDmg
+                        print("Success!")
+                    else:
+                        health -= smiteDmg
+                        print("KEKW")
 
 
     buffer = 2
     if (x > mX + buffer or x < mX - buffer) and (y < mY - buffer or y > mY + buffer):
         x += xVel
         y += yVel
-    healthbar(health)
+
     screen.blit(playerImage, (x,y))
-    screen.blit(baronImage, (530, 300))
+
     pygame.display.update()
