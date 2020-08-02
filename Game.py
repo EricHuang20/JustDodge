84c5ed8324b1e2ad8b771d0ddb8ae7ddd6660e57
EricHuang20 <sphericyt@gmail.com>
2020-08-02 19:54:46 -0400

    Added comments and fixed movement after arriving bug

--- Game.py
+++ Game.py
@@ -1,11 +1,12 @@
 import pygame
 import random
 
+#Initialization of Variables
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption('Just Dodge 4Head')
 playerImage = pygame.image.load("player.png")
-playerImage = pygame.transform.scale(playerImage, (50, 50))
+playerImage = pygame.transform.scale(playerImage, (60, 80))
 baronImage = pygame.image.load("baron.png")
 baronImage = pygame.transform.scale(baronImage, (200, 300))
 
@@ -17,18 +18,23 @@ x = 400
 y = 400
 xVel = 0
 yVel = 0
-speed = 0.3
+speed = 0.5
 xDist = 0
 yDist = 0
 mX = 0
 mY = 0
 
+arrived = False
+
+
+#
 def convert(variable, leftMin, leftMax, rightMin, rightMax):
     leftSpan = leftMax - leftMin
     rightSpan = rightMax - rightMin
     valueConverted = float(variable - leftMin) / float(leftSpan)
     return rightMin + (valueConverted * rightSpan)
 
+#Draws the health bar above Baron Nashor
 def healthbar(curHealth):
     damageScale = convert(curHealth, 1, 10000, 0, 248)
     pygame.draw.rect(screen, (255,255,255), (500,200,250,50), 2)
@@ -37,14 +43,18 @@ def healthbar(curHealth):
     screen.blit(healthText, (575, 150))
     screen.blit(baronImage, (530, 300))
 
+#Clears the Screen
 def removeBaron():
     pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 800), 0)
 
-
+#Gets the velocities needed to move the player to the mouse location
 def move(xDist, yDist, spd):
 
+    #Sees which direction the player has to travel further in
     maximum = max(abs(xDist), abs(yDist))
 
+    #Creates separate x and y velocities based on the ratio of the x and y distances travelled to
+    #ensure that the diagonal speed will allow the player to move to the mouse location
     if maximum == abs(xDist):
         ratio = (abs(xDist) / abs(yDist)) + 1
         print(ratio)
@@ -90,17 +100,20 @@ def move(xDist, yDist, spd):
                     return xVel, yVel
 
 
-active = True
 
 
 timeElapsed = 0
 clock = pygame.time.Clock()
+
+#Game loop
+active = True
 while active:
     mX, mY = pygame.mouse.get_pos()
     screen.fill((0,0,0))
     dt = clock.tick()
     healthbar(health)
 
+    #Makes baron take random amounts of damage constantly
     timeElapsed += dt
     if health > 0:
         if timeElapsed > 300:
@@ -112,7 +125,11 @@ while active:
         smiteable = False
         removeBaron()
 
+    #Resets player velocity if the player arrives at the click location
+    if arrived == True:
+        xVel, yVel = 0, 0
 
+    #Checks for player inputs
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
@@ -133,12 +150,15 @@ while active:
                         health -= smiteDmg
                         print("KEKW")
 
-
+    #Moves player to mouse click location with a buffer of 2 pixels (accounts for marginal error in velocity) if they have not arrived
     buffer = 2
     if (x > mX + buffer or x < mX - buffer) and (y < mY - buffer or y > mY + buffer):
+        arrived = False
         x += xVel
         y += yVel
+    else:
+        arrived = True
 
-    screen.blit(playerImage, (x,y))
+    screen.blit(playerImage, (x-30,y-40))
 
     pygame.display.update()
